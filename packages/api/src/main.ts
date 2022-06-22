a648d6cb9d18677634f5528b3f1fdb06a17a9221
KRaken <kuzembay@gmail.com>
2022-06-22 14:58:28 +0600

    feat: everything in middleware

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -5,10 +5,12 @@ import * as mongoose from 'mongoose'
 import * as UserRoutes from './api/users'
 import * as PostRoutes from './api/posts'
 import * as AuthRoutes from './api/auth'
-import { verifyToken } from './middleware/auth'
+import { cors, handler, responseTimer, verifyToken } from './middleware/auth'
 
 const app = new Koa()
 const router = new Router()
+
+app.use(cors)
 
 app.use(
   bodyParser({
@@ -19,23 +21,9 @@ app.use(
   })
 )
 
-// logger
-app.use(async (ctx, next) => {
-  console.log(`${ctx.method} ${ctx.url}`)
-  await next()
-  const rt = ctx.response.get('X-Response-Time')
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`)
-})
-
-// x-response-time
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.set('X-Response-Time', `${ms}ms`)
-})
-
+app.use(responseTimer)
 app.use(verifyToken)
+app.use(handler)
 
 app
   .use(AuthRoutes.router.routes())
--- packages/api/src/middleware/auth.ts
+++ packages/api/src/middleware/auth.ts
@@ -1,11 +1,27 @@
 import * as jwt from 'jsonwebtoken'
+import { errorHandler } from '@next-electron-app/utils'
+
+export const cors = async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', 'http://localhost:4200')
+  ctx.set('Access-Control-Allow-Headers', '*')
+  await next()
+}
+
+// x-response-time
+export const responseTimer = async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+}
 
 export const verifyToken = async (ctx, next) => {
   if (ctx.url !== '/login' && ctx.url !== '/register') {
     const token =
-      ctx.request.body.token ||
-      ctx.request.query.token ||
-      ctx.request.headers['x-access-token']
+      ctx.request?.body?.token ||
+      ctx.request?.query?.token ||
+      ctx.request?.headers['x-access-token']
 
     if (!token) {
       ctx.throw(403, 'A token is required for authentication')
@@ -18,4 +34,16 @@ export const verifyToken = async (ctx, next) => {
     }
   }
   await next()
+}
+
+export const handler = async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    console.log({ err, data: ctx.request.body || ctx.params })
+    const result = errorHandler(err)
+
+    ctx.body = { message: result.message }
+    ctx.status = result.status || 500
+  }
 }
